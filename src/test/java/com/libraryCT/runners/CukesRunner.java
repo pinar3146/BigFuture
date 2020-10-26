a025cff68ad85b7b88b7dfad03806ece2f1931ea
pinar3146 <pinaryanar3146@hotmail.com>
2020-10-26 22:06:42 +0100

    first commit

---
+++ src/test/java/com/libraryCT/runners/CukesRunner.java
@@ -0,0 +1,20 @@
+package com.libraryCT.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                  "html:target/default-html-reports"
+         },
+
+        features = "src/test/resources/features",
+        glue = "com/libraryCT/stepdef",
+        dryRun = false,
+        tags = "@wip"
+)
+public class CukesRunner {
+}
