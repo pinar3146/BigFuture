bcbd7128644615c56673f3dafede3cb6ad680c3d
Caglar Keskin <caglarkeskin@gmail.com>
2020-11-02 20:01:10 +0100

    Login Page POM is updated

--- src/test/java/com/libraryCT/pages/LoginPage.java
+++ src/test/java/com/libraryCT/pages/LoginPage.java
@@ -1,4 +1,26 @@
 package com.libraryCT.pages;
 
+import com.libraryCT.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+    public LoginPage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+
+    @FindBy(id = "inputEmail")
+    public WebElement inputEmailBox;
+
+    @FindBy(id = "inputPassword")
+    public WebElement inputPasswordBox;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement signInButton;
+
+
+
+
 }
