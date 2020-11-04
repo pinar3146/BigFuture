078eb005ac1d600e583dcac30dc7b85db66f1242
pinar3146 <pinaryanar3146@hotmail.com>
2020-11-04 17:24:03 +0100

    commit

--- src/test/java/com/libraryCT/pages/BorrowingBooksPage.java
+++ src/test/java/com/libraryCT/pages/BorrowingBooksPage.java
@@ -1,5 +1,34 @@
 package com.libraryCT.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
 public class BorrowingBooksPage {
-    //new package
+@FindBy(xpath = "(//span[@class=\'title\'][contains(.,\'Borrowing Books\')])")
+    public WebElement borrowingBooks;
+
+@FindBy(xpath = "//div[@class='card-header'][contains(.,'Boorrowed Books')]")
+    public WebElement borrowedBooks;
+@FindBy(xpath = "//th[contains(.,'Book Name')]")
+public List<WebElement> rowNames;
+
+@FindBy(xpath = "//td[@colspan='6'][contains(.,'There is no book borrowed by this user.')]")
+    public WebElement shownText;
+@FindBy(xpath = "//select[contains(@class,'form-control select2')]")
+    public WebElement userName;
+@FindBy(xpath = "//label[@class='control-label col-md-4'][contains(.,'Users')]")
+    public WebElement users;
+
+
+
+
+
+
+
+
+
 }
